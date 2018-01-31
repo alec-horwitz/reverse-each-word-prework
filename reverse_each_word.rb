493de68a869e44920aa808aaf66e5cb42c50ce8c
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-31 03:08:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   temp = string.split(" ")
   temp2 = Array.new
-  temp.each { |word|  
+  temp.each { |word|
     temp2.unshift(word)
   }
-  string = temp2.unsplit(" ")
+  string = temp2.join(" ")
 end
